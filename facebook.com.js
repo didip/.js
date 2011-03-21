1d86f0ac31a91b808572e59260085d4cd7c67468
Didip Kerabat <didipk@gmail.com>
2011-03-20 17:34:40 -0700

    touch up for facebook

--- facebook.com.js
+++ facebook.com.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
   $("#pagelet_groups_nav").remove();
   $("#pagelet_apps_nav").remove();
-  $("#chatFriendsOnline").remove();
-  $(".fbDockWrapper.fbDockWrapperBottom.fbDockWrapperRight").remove();
-  $(".uiComposerMessageBox .composerTypeahead .wrap textarea")
-    .css('font-size', '22px')
-    .css('height', '25px');
 
-  $(".uiComposerMessageBox .composerTypeahead .wrap")
-    .css('height', '25px');
+  $(".uiComposerMessageBox .composerTypeahead .wrap").css('height', '25px');
+  $(".uiComposerMessageBox .composerTypeahead .wrap textarea").css({
+    'font-size': '22px',
+    'height': '25px'
+  });
 
   $("#rightCol").remove();
   $("#home_sponsor_nile").remove();
   $("#contentArea").css('width', '95%');
-  $(".uiUfi").css('width', '100%');
-  $(".commentContent").css('font-size', '14px').css('line-height', '1.6');
+
+  setInterval(function() {
+    $(".uiUfi").css('width', '100%');
+    $(".commentContent, .messageBody").css('font-size', '14px').css('line-height', '1.6');
+  }, 5000);
+
 });
