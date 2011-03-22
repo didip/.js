6e5fafc0f4c555eb4e18a77bb182cf6060e507c2
Didip Kerabat <didipk@gmail.com>
2011-03-21 22:31:57 -0700

    more reddit fixes

--- reddit.com.js
+++ reddit.com.js
@@ -17,9 +17,6 @@ $(document).ready(function() {
       'border-left': '1px dotted #ddf',
       'margin-left': '7px'
     });
-
-    // Sidebar work
-    $(".side .spacer:first").remove();
   }
 
   // header work
@@ -33,18 +30,17 @@ $(document).ready(function() {
   // header-right work
   $("#header-bottom-right").css({
     'background': 'none',
-    'border': 'none'
+    'border': 'none',
+    'margin-right': '5px'
   });
   $("#header-bottom-right .pref-lang").css({'font-weight': 'normal'});
   $("#header-bottom-right .submit, #header-bottom-right .user, #header-bottom-right .user, #header-bottom-right .flat-list.hover, #header-bottom-right .logout").css({
     'margin-left': '12px',
     'color': '#777'
   });
-  $("#header-bottom-right .logout").css({
-    'margin-right': '12px'
-  });
   $("#header-bottom-right .nohavemail").css({
-    'margin-left': '4px'
+    'margin-left': '4px',
+    'vertical-align': '-1px'
   });
   $("#header-bottom-right .separator").remove();
 
@@ -58,14 +54,20 @@ $(document).ready(function() {
   });
   $(".side .spacer .morelink .nub").remove();
   $(".side .spacer .sidebox .spacer").remove();
+  $(".side .spacer .linkinfo").parent().remove();
+  $(".side .spacer .sidecontentbox").parent().remove();
 
   var random_button = '<div class="spacer"><div class="sidebox"><div class="morelink"><a href="http://www.reddit.com/r/random">Random Reddit</a></div></div></div>';
   $(random_button).insertAfter($(".side .spacer .sidebox.submit").parent());
 
-  $(".side .spacer .morelink").css({
+  $(".side .spacer .morelink, #search input[type=text]").css({
+    'height': '25px',
     '-moz-border-radius': '3px',
     '-webkit-border-radius': '3px',
     'border-radius': '3px'
+  });
+  $("#search input[type=text]").css({
+    'border': '1px solid #ccc'
   });
 
   $(".side .spacer .morelink:hover a").css({
@@ -73,6 +75,58 @@ $(document).ready(function() {
   });
 
   $(".side .spacer .sidecontentbox").parent().remove();
+
+  // login form
+  $("#login_login-main").css({
+    'margin': '18px 0 0',
+    'padding': '5px 0 4px',
+    'border': 'none',
+    'border-top': '1px dotted #ddd',
+    'border-bottom': '1px dotted #ddd'
+  });
+  $("#login_login-main input[name=user], #login_login-main input[type=password]").css({
+    'height': '19px',
+    'border': '1px solid #ccc',
+    '-moz-border-radius': '3px',
+    '-webkit-border-radius': '3px',
+    'border-radius': '3px'
+  });
+
+  $("#login_login-main input[name=user]").attr("placeholder", "username");
+  $("#login_login-main input[type=password]").attr("placeholder", "*********");
+  $("#login_login-main button").addClass("morelink").css({
+    'color': '#336699',
+    'padding': '0 8px 3px',
+    'height': '32px',
+    'vertical-align': 'center',
+    '-moz-border-radius': '3px',
+    '-webkit-border-radius': '3px',
+    'border-radius': '3px'
+  });
+  $("#login_login-main .recover-password").remove();
+
+  $("label[for='rem-login-main']").css({
+    'color': '#666'
+  });
+
+  $(".titlebox h1").css({
+    'margin': '0 0 1px'
+  });
+
+  $(".titlebox .usertext-body .md").css({
+    'font-size': '12px',
+    'margin-top': '0px'
+  });
+
+  $(".titlebox .usertext-body .md p").css({
+    'margin': '0 0 7px'
+  });
+
+  $(".fancy-toggle-button .active, .fancy-toggle-button .remove").css({
+    'padding': '2px 8px',
+  });
+  $(".fancy-toggle-button").siblings(".number, .word").remove();
+
 
   // tab work
   $(".tabmenu li:last").remove();
@@ -117,12 +171,35 @@ $(document).ready(function() {
   $("#siteTable .link .title").css({
     'margin-bottom': '2px'
   });
-  $("#siteTable .link .title a").css({
-    'color': '#369'
-  });
+
   $("#siteTable .link .title a").attr("target", "_blank");
   $("#siteTable .link .tagline").css({
     'margin-bottom': '1px'
+  });
+
+  // textarea
+  $(".usertext-edit").css('width', '600px');
+  $("textarea[name=text]").css({
+    'width': '600px',
+    'height': '300px',
+    'border': '1px solid #aaa',
+    '-moz-border-radius': '3px',
+    '-webkit-border-radius': '3px',
+    'border-radius': '3px'
+  });
+  $(".usertext-edit button.save").addClass('morelink').css({
+    'color': '#336699',
+    'padding': '0 8px 3px',
+    'height': '32px',
+    'vertical-align': 'center',
+    '-moz-border-radius': '3px',
+    '-webkit-border-radius': '3px',
+    'border-radius': '3px'
+  });
+
+  $(".nextprev").css({
+    'font-size': '35px',
+    'margin': '10px'
   });
 
   // footer work
@@ -139,6 +216,7 @@ $(document).ready(function() {
   new_footer += '<li><a href="http://www.reddit.com/bookmarklets/">bookmarklets</a></li>';
   new_footer += '<li><a href="http://www.reddit.com/socialite/">socialite</a></li>';
   new_footer += '<li><a href="http://www.reddit.com/code/">reddit code</a></li>';
+  new_footer += '<li><a href="http://www.reddit.com/password">recover password</a></li>';
   new_footer += '</ul>';
 
   $(".footer-parent .footer").removeClass('rounded');
